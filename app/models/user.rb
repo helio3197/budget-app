b956462480c145a9b54ef73bcdd4db83256c7ec7
Kenny <kennysalazar3197@gmail.com>
2022-06-27 18:16:47 -0500

    Initialize active_storage and run migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validates :avatar, file_size: { less_than_or_equal_to: 5.megabytes },
+                     file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
 end
