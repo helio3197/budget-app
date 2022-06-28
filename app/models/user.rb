c0130ddaaca288deb393a8ec516b1dfab0f0af7b
Kenny <kennysalazar3197@gmail.com>
2022-06-27 22:18:13 -0500

    Add profile picture input handler

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  validates :name, presence: true, length: { maximum: 50 }
+
   validates :avatar, file_size: { less_than_or_equal_to: 5.megabytes },
                      file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    get 'account', to: 'devise/registrations#edit'
+    get 'password/new', to: 'devise/passwords#new'
+    get 'password/edit', to: 'devise/passwords#edit'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
