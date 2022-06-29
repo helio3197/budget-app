bc579b4b54d214ce1827c4dbd98c67bc05354849
Kenny <kennysalazar3197@gmail.com>
2022-06-28 19:37:40 -0500

    Add validations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :operations
-
   has_one_attached :icon
+  belongs_to :user
+
+  validates_associated :user, :operations
+  validates :name, presence: true, length: { maximum: 50 }
 
   validates :icon, file_size: { less_than_or_equal_to: 1.megabytes },
                    file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :categories, :operations
 
   validates :name, presence: true, length: { maximum: 50 }
 
