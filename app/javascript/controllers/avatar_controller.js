f0c8379452df68c98242762b531140526bae80dd
Kenny <kennysalazar3197@gmail.com>
2022-06-29 22:11:29 -0500

    Update edit avatar section

--- app/javascript/controllers/avatar_controller.js
+++ app/javascript/controllers/avatar_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['preview', 'picture', 'fileName', 'clearBtn']
+  static targets = ['preview', 'picture', 'fileName', 'clearBtn', 'placeHolderPic', 'removeAvatarBtn']
 
   connect() {
     this.defaultAvatar = this.previewTarget.src
@@ -12,16 +12,34 @@ export default class extends Controller {
       this.previewTarget.src = URL.createObjectURL(this.picture)
       this.fileNameTarget.textContent = this.picture.name
       this.clearBtnTarget.className = ''
+      if (this.avatarRemoved) {
+        document.getElementById('remove_avatar').remove()
+      }
     } else {
       this.previewTarget.src = this.defaultAvatar
       this.fileNameTarget.textContent = 'No file chosen...'
       this.clearBtnTarget.className = 'd-none'
+      if (this.avatarRemoved) {
+        this.removeAvatar()
+      }
     }
   }
 
   destroy() {
     this.pictureTarget.value = ''
     this.update()
+  }
+
+  removeAvatar() {
+    this.previewTarget.src = this.placeHolderPicTarget.textContent
+    this.removeAvatarBtnTarget.className = 'd-none'
+    const removeAvatarInput = document.createElement('input')
+    removeAvatarInput.type = 'hidden'
+    removeAvatarInput.name = 'user[remove_avatar]'
+    removeAvatarInput.value = 'true'
+    removeAvatarInput.id = 'remove_avatar'
+    document.getElementById('edit_user').appendChild(removeAvatarInput)
+    this.avatarRemoved = true
   }
 
   get picture() {
