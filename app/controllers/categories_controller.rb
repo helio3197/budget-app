381df3f3f9d0b818c2e709b3b2c6118d55cebedc
Kenny <kennysalazar3197@gmail.com>
2022-06-30 01:28:57 -0500

    Create new category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :auhtenticate_user!, except: %i[index]
+  before_action :authenticate_user!, except: %i[index]
 
   def index
     if user_signed_in?
@@ -11,5 +11,22 @@ class CategoriesController < ApplicationController
 
   def new
     @category = Category.new
+  end
+
+  def create
+    # @category = current_user.categories.new(category_params)
+    @category = Category.new(**category_params, user: current_user)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Category created successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit :name, :icon
   end
 end
