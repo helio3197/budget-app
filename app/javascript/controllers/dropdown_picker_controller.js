f9f241bdce480229c138ff8152b0a71249482764
Kenny <kennysalazar3197@gmail.com>
2022-07-01 13:46:25 -0500

    Update dropdown handler stimulus controller

--- app/javascript/controllers/dropdown_picker_controller.js
+++ app/javascript/controllers/dropdown_picker_controller.js
@@ -7,11 +7,15 @@ export default class extends Controller {
     this.dispatch('select', { detail: id })
     currentTarget.parentElement.hidden = true
     this.dispatch('updated')
+    const categories = this.categoriesInputTarget.value
+    this.categoriesInputTarget.value = categories ? JSON.stringify([...JSON.parse(categories), id]) : JSON.stringify([id])
   }
 
   unselect({ detail: id }) {
     [...this.dropdownListTarget.children].find((element) => element.dataset.id === id.toString()).hidden = false
     this.dispatch('updated')
+    const categories = JSON.parse(this.categoriesInputTarget.value).filter((v) => v !== id)
+    this.categoriesInputTarget.value = categories.length ? JSON.stringify(categories) : ''
   }
 
   updateDropdownList() {
