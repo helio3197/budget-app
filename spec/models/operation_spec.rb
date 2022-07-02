6251a774dc24295b063904aaf0557ece365e38f9
Kenny <kennysalazar3197@gmail.com>
2022-07-02 00:29:13 -0500

    Update unit tests for models

--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Operation, type: :model do
   subject do
     u = User.create(name: 'Jhon', email: 'test123@test.com', password: 'abc123')
-    described_class.new(user: u, name: 'Test', amount: 25)
+    c = [Category.new(name: 'Test Cat', user: u, icon: fixture_file_upload('test_icon.png', 'image/png'))]
+    described_class.new(user: u, name: 'Test', amount: 25, categories: c)
   end
 
   it 'Should be valid with valid attributes' do
@@ -17,6 +18,11 @@ RSpec.describe Operation, type: :model do
 
   it 'Should not be valid without an amount' do
     subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Should not be valid with an invalid amount' do
+    subject.amount = -23
     expect(subject).to_not be_valid
   end
 
