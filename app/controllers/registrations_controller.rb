4fbb876d35075a0365e8bff47bf752ac3e20922e
Kenny <kennysalazar3197@gmail.com>
2022-07-04 01:24:12 -0500

    Update registrations#update controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   def update
-    params = if account_update_params[:remove_avatar]
-               account_update_params.except :remove_avatar
-             else
-               account_update_params
-             end
-
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
-    resource_updated = update_resource(resource, params)
+    resource_updated = update_resource(resource, account_update_params.except(:remove_avatar))
     yield resource if block_given?
     if resource_updated
-      current_user.avatar.purge
+      current_user.avatar.purge if account_update_params[:remove_avatar]
       set_flash_message_for_update(resource, prev_unconfirmed_email)
       bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
 
