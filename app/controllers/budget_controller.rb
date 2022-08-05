05839576f6b6fc609a86ec2bd6bad04a79256301
Kenny <kennysalazar3197@gmail.com>
2022-08-04 19:44:06 -0500

    Create exec_depsoit action for the bufget controller

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -3,14 +3,34 @@ class BudgetController < ApplicationController
   end
 
   def new_deposit
+    @operation = Operation.new
   end
 
   def exec_deposit
+    @operation = balance_operation 'Deposit'
+
+    @operation.transaction do
+      @operation.save!
+      current_user.update! balance: current_user.balance + operation_params[:amount].to_f
+    rescue ActiveRecord::RecordInvalid
+      render :new_deposit, status: :unprocessable_entity and return
+    end
+    redirect_to my_budget_path, notice: 'Deposit successful!'
   end
 
   def new_withdraw
   end
 
   def exec_withdraw
+  end
+
+  private
+
+  def balance_operation(name)
+    Operation.new(**operation_params, name:, categories: [current_user.categories.first], user: current_user)
+  end
+
+  def operation_params
+    params.require(:operation).permit(:amount, :description)
   end
 end
