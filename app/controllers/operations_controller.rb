5887a8da9aa12bba3c44f55f6e886fd7406cbc97
Kenny <kennysalazar3197@gmail.com>
2022-08-05 19:20:12 -0500

    Update operation mvc

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -8,27 +8,37 @@ class OperationsController < ApplicationController
   end
 
   def create
+    user = current_user
     parsed_params = operation_params
     parsed_params[:categories] = if parsed_params[:categories].nil? || parsed_params[:categories].empty?
                                    [Category.find(params[:category_id])]
                                  else
                                    Category.find(*JSON.parse(parsed_params[:categories]) << params[:category_id])
                                  end
-    @operation = Operation.new(**parsed_params, user: current_user)
+    operation_amount = parsed_params[:amount].to_f
+    @operation = Operation.new(**parsed_params, amount: -operation_amount, user:)
 
-    if @operation.save
-      redirect_to category_path(params[:category_id]), notice: 'Transaction created successfully.'
-    else
-      @current_category = Category.find(params[:category_id])
-      @others_categories = current_user.categories.includes([icon_attachment: :blob]).where
-        .not(id: params[:category_id])
-      render :new, status: :unprocessable_entity
+    @operation.transaction do
+      @operation.save!
+      raise ActiveRecord::RecordInvalid if amount_negative? operation_amount
     end
+
+    redirect_to category_path(params[:category_id]), notice: 'Transaction created successfully.'
+  rescue ActiveRecord::RecordInvalid
+    @operation.invalidate_negative_amount if amount_negative? operation_amount
+    @current_category = Category.find(params[:category_id])
+    @others_categories = user.categories.includes([icon_attachment: :blob]).where
+      .not(id: params[:category_id])
+    render :new, status: :unprocessable_entity
   end
 
   private
 
   def operation_params
     params.require(:operation).permit :name, :amount, :categories, :description
+  end
+
+  def amount_negative?(value)
+    value&.to_f&.negative?
   end
 end
