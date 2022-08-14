167a6709cb21e8bedc17ca0230c9bb7f1832c585
Kenny <kennysalazar3197@gmail.com>
2022-08-14 15:20:14 -0500

    Update utils controller

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -2,7 +2,13 @@ class UtilsController < ApplicationController
   def fetch_image
     img_url = params[:img]
 
+    if params[:img].nil? || !params[:img].match?(%r{\Ahttps?://(?:[\w-]+\.)?[\w-]+\.\w+(?:/.+)\z})
+      head(:bad_request) and return
+    end
+
     res = Net::HTTP.get_response(URI(img_url))
+
+    head(:not_found) and return unless res.is_a?(Net::HTTPSuccess) || res.is_a?(Net::HTTPOK)
 
     send_data res.read_body, filename: 'img', type: res.content_type, disposition: 'inline'
   end
