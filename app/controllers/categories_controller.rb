179be9bf173f4a70757bf1ee3df63f71b100ecbc
Kenny <kennysalazar3197@gmail.com>
2022-08-17 19:34:26 -0500

    Update categories#destroy action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -58,7 +58,7 @@ class CategoriesController < ApplicationController
     category = current_user.categories.find(params[:id])
     category.destroy
 
-    redirect_to root_path, notice: 'Category deleted successfully.'
+    redirect_to root_path, notice: 'Category deleted successfully.', status: :see_other
   end
 
   private
